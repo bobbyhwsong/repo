530a2302c758fbf293553e0ff1a4d72350571b61
HyungWoo Song <rotto95@snu.ac.kr>
2023-02-09 23:04:23 +0900

    ss

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,36 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 DIVIDER = '--------------------------------------------------------------------------------'
 
+@st.cache
+def load_data():
+        data = pd.read_csv('Superstore.csv')
+        data2 = pd.read_excel('Super.xls',sheet_name='반품 정리')
+        data2 = data2.drop_duplicates()
+        df_merge = pd.merge(data, data2, how='left', on='주문 ID')
+        df_merge = df_merge.drop(['행 ID','고객 이름','제품 이름'],axis=1)
+        df = df_merge
+        
+        df.rename(columns={'주문 날짜':'OrderDate'},inplace=True)
+        df.rename(columns={'배송 날짜':'ShipDate'},inplace=True)
+
+        df['주문 ID']= df['주문 ID'].astype('str')
+        df['OrderDate'] = pd.to_datetime(df['OrderDate'])
+        df['ShipDate'] = pd.to_datetime(df['ShipDate'])
+        df['배송 형태']= df['배송 형태'].astype('str')
+        df['고객 ID']= df['고객 ID'].astype('str')
+        df['세그먼트'] = df['세그먼트'].astype('str')
+        df['도시'] = df['도시'].astype('str')
+        df['시/도'] = df['시/도'].astype('str')
+        df['국가/지역'] = df['국가/지역'].astype('str')
+        df['지역'] = df['지역'].astype('str')
+        df['제품 ID'] = df['제품 ID'].astype('str')
+        df['범주'] = df['범주'].astype('str')
+        df['하위 범주'] = df['하위 범주'].astype('str')
+        
+        df.fillna('no',inplace=True)
+        df = df[df['반품']=='no']
+        return df
+
 ####################################
 # - Sidebar
 ####################################
@@ -16,7 +46,7 @@ with st.sidebar:
     st.title("비저블 지원")
     st.markdown("**- 기획서 :** 기획서 프로세스")
     st.markdown("**- 대시보드 :** 대시보드 스케치")
-
+    
 VIEW_PROPOSAL = '기획서'
 VIEW_DASHBOARD = '대시보드'
 
@@ -29,7 +59,15 @@ with st.sidebar:
         "Choose a language (not working)",
         ("English", "Korean")
     )
+    
+data_load_state = st.text('Loading data...')
+data = load_data()
+data_load_state.text("데이터 불러오기 완료!")
 
+if st.checkbox('데이터 보기'):
+        st.subheader('Superstore.csv')
+        st.write(df)
+        
 ####################################
 # - VIEW_PROPOSAL
 ####################################
@@ -38,8 +76,12 @@ if add_sidebar == VIEW_PROPOSAL:
     st.write(DIVIDER)
     
     st.subheader('1. 문제 정의')
-    st.markdown('##### 동남아시아 지역에서 발생하는 매출에 비해 수익이 현저히 적은 것을 볼 수 있다.')
-    st.markdown('##### 이에 동남아시아 지역에서의 수익구조를 개선할 필요가 있다.')
+    st.markdown('''##### 동남아시아 지역에서 발생하는 매출에 비해 수익이 현저히 적은 것을 볼 수 있다. 이에 동남아시아 지역에서의 수익구조를 개선할 필요가 있다.''')
+    
+    st.write('''fig = px.pie(data,values='매출',names='지역', title='지역 별 매출 비중', color_discrete_sequence=px.colors.sequential.RdBu)
+             fig.update_traces(textposition='inside', textinfo='percent+label')
+             fig.show()''')
+    
     st.write(DIVIDER)
     
     st.subheader('2. 지표 설정')
